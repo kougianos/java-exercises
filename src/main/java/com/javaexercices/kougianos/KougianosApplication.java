524a82392fe7b302f372f268acdfe30a567448d6
Nikos Kougianos <kougi_jr@hotmail.com>
2020-11-17 18:03:10 +0200

    Initial setup

---
+++ src/main/java/com/javaexercices/kougianos/KougianosApplication.java
@@ -0,0 +1,13 @@
+package com.javaexercices.kougianos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KougianosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KougianosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaexercices/kougianos/KougianosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaexercices.kougianos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KougianosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
