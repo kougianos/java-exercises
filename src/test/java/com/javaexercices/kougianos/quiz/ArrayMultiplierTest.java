44968cd05458f30c78da822e970fd23b6a584c57
Nikos Kougianos <kougi_jr@hotmail.com>
2020-11-18 22:52:44 +0200

    ArrayMultiplier challenge

---
+++ src/test/java/com/javaexercices/kougianos/quiz/ArrayMultiplierTest.java
@@ -0,0 +1,38 @@
+package com.javaexercices.kougianos.quiz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class ArrayMultiplierTest {
+
+    @Test
+    public void test01() {
+        assertArrayEquals(new Object[][]{{4,4},{5,5}}, ArrayMultiplier.multiply(new Object[]{4,5}));
+    }
+
+    @Test
+    public void test02() {
+        assertArrayEquals(new Object[][]{{1}}, ArrayMultiplier.multiply(new Object[]{1}));
+    }
+
+    @Test
+    public void test03() {
+        assertArrayEquals(new Object[][]{{"*","*","*"},{"%","%","%"},{"$","$","$"}}, ArrayMultiplier.multiply(new Object[]{"*","%","$"}));
+    }
+
+    @Test
+    public void test04() {
+        assertArrayEquals(new Object[][]{{"A","A","A","A","A"},{"B","B","B","B","B"},{"C","C","C","C","C"},{"D","D","D","D","D"},{"E","E","E","E","E"}}, ArrayMultiplier.multiply(new Object[]{"A","B","C","D","E"}));
+    }
+
+    @Test
+    public void test05() {
+        assertArrayEquals(new Object[][]{{"x","x","x"},{"y","y","y"},{"z","z","z"}}, ArrayMultiplier.multiply(new Object[]{"x","y","z"}));
+    }
+
+    @Test
+    public void test06() {
+        assertArrayEquals(new Object[][]{{1,1},{0,0}}, ArrayMultiplier.multiply(new Object[]{1,0}));
+    }
+}
--- src/test/java/com/javaexercices/kougianos/quiz/BrickTest.java
+++ src/test/java/com/javaexercices/kougianos/quiz/BrickTest.java
@@ -1,6 +1,7 @@
 package com.javaexercices.kougianos.quiz;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
