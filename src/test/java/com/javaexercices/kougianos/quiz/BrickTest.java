4b36361ad493456118fe4339731b42e91d663ca8
Nikos Kougianos <kougi_jr@hotmail.com>
2020-11-18 20:20:54 +0200

    Brick challenge

---
+++ src/test/java/com/javaexercices/kougianos/quiz/BrickTest.java
@@ -0,0 +1,38 @@
+package com.javaexercices.kougianos.quiz;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class BrickTest {
+
+    @Test
+    public void test1() {
+        assertEquals(true, Brick.doesBrickFit(1,1,1, 1,1));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(true, Brick.doesBrickFit(1,2,1, 1,1));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(false, Brick.doesBrickFit(1,2,2, 1,1));
+    }
+
+    @Test
+    public void test4() {
+        assertEquals(true, Brick.doesBrickFit(1,2,2, 1,2));
+    }
+
+    @Test
+    public void test5() {
+        assertEquals(true, Brick.doesBrickFit(1,2,2, 2,1));
+    }
+
+    @Test
+    public void test6() {
+        assertEquals(false, Brick.doesBrickFit(2,2,2, 1,2));
+    }
+}
