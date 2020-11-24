ed7f85eaf11acd7d65a332c3b651f910f7624746
Nikos Kougianos <kougi_jr@hotmail.com>
2020-11-24 23:32:31 +0200

    ConsecutiveNumbers challenge

---
+++ src/main/java/com/javaexercices/kougianos/quiz/ConsecutiveNumbers.java
@@ -0,0 +1,39 @@
+package com.javaexercices.kougianos.quiz;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Create a function that determines whether elements in an array can be re-arranged
+ * to form a consecutive list of numbers where each number appears exactly once.
+ * <p>
+ * cons([5, 1, 4, 3, 2]) ➞ true
+ * // Can be re-arranged to form [1, 2, 3, 4, 5]
+ * <p>
+ * cons([5, 1, 4, 3, 2, 8]) ➞ false
+ * <p>
+ * cons([5, 6, 7, 8, 9, 9]) ➞ false
+ * // 9 appears twice
+ */
+public class ConsecutiveNumbers {
+
+    public static boolean cons(int[] arr) {
+
+        List<Integer> list = Arrays.stream(arr).boxed().sorted()
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < list.size(); i++) {
+            if (i == list.size() - 1) {
+                break;
+            }
+            if (list.get(i) + 1 != list.get(i + 1)) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+}
