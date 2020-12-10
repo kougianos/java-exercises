0fe718a69fa8fab6d15ea44ea369639d5a746c3e
Nikos Kougianos <kougi_jr@hotmail.com>
2020-12-10 23:08:31 +0200

    CapitalChallenge challenge

---
+++ src/main/java/com/javaexercices/kougianos/quiz/CapitalChallenge.java
@@ -0,0 +1,54 @@
+package com.javaexercices.kougianos.quiz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given two strings, s1 and s2, select only the characters in each string where the
+ * character in the same position in the other string is in uppercase. Return these as a single string.
+ * <p>
+ * To illustrate, given the strings s1 = "heLLo" and s2 = "GUlp", we select the
+ * letters "he" from s1, because "G" and "U" are uppercase. We then select "lp" from s2,
+ * because "LL" is in uppercase. Finally, we join these together and return "help".
+ * <p>
+ * selectLetters("heLLO", "GUlp") ➞ "help"
+ * selectLetters("1234567", "XxXxX")  ➞ "135"
+ * selectLetters("EVERYTHING", "SomeThings") ➞  "EYSomeThings"
+ */
+public class CapitalChallenge {
+
+    public static void main(String[] args) {
+        String result = selectLetters("EVERYTHING", "SomeThings");
+    }
+
+    public static String selectLetters(String s1, String s2) {
+
+        int length = Math.max(s1.length(), s2.length());
+        List<Integer> capitals1 = new ArrayList<>();
+        List<Integer> capitals2 = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s2.length(); i++) {
+            try {
+                if(Character.isUpperCase(s2.charAt(i))) {
+                    sb.append(s1.charAt(i));
+                }
+            } catch (IndexOutOfBoundsException e) {
+                // do nothing
+            }
+        }
+        for (int i = 0; i < s1.length(); i++) {
+            try {
+                if(Character.isUpperCase(s1.charAt(i))) {
+                    sb.append(s2.charAt(i));
+                }
+            } catch (IndexOutOfBoundsException e) {
+                // do nothing
+            }
+        }
+
+        return String.valueOf(sb);
+
+    }
+
+}
---
+++ src/test/java/com/javaexercices/kougianos/quiz/CapitalChallengeTest.java
@@ -0,0 +1,38 @@
+package com.javaexercices.kougianos.quiz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CapitalChallengeTest {
+    @Test
+    public void test1() {
+        assertEquals("help", CapitalChallenge.selectLetters("heLLO", "GUlp"));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals("135", CapitalChallenge.selectLetters("1234567", "XxXxX"));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals("EYSomeThings", CapitalChallenge.selectLetters("EVERYTHING", "SomeThings"));
+    }
+
+    @Test
+    public void test4() {
+        assertEquals("atrpten", CapitalChallenge.selectLetters("PaTtErN", "pAtTeRn"));
+    }
+
+    @Test
+    public void test5() {
+        assertEquals("", CapitalChallenge.selectLetters("nothing", "nothing"));
+    }
+
+    @Test
+    public void test6() {
+        assertEquals("What doR", CapitalChallenge.selectLetters("What do you think of it so far?", "RUBBISH!"));
+    }
+}
+    
