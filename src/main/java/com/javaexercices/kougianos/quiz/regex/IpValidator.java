a03bf75c8d1b1b45b2e8a77d794e8f8cbace7e56
Nikos Kougianos <kougi_jr@hotmail.com>
2021-02-27 15:11:12 +0200

    Regex - IP Validator

---
+++ src/main/java/com/javaexercices/kougianos/quiz/regex/IpValidator.java
@@ -0,0 +1,29 @@
+package com.javaexercices.kougianos.quiz.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class IpValidator {
+
+    public static boolean isValidIPAddress(String ip) {
+
+        if (ip == null) {
+            return false;
+        }
+
+        // Regex for digit from 0 to 255.
+        String zeroTo255 = "(\\d{1,2}|([01])\\d{2}|2[0-4]\\d|25[0-5])";
+
+        // Regex for a digit from 0 to 255 and
+        // followed by a dot, repeat 4 times.
+        String regex = zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255;
+
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(ip);
+
+        // Return if the IP address
+        // matched the ReGex
+        return m.matches();
+    }
+
+}
