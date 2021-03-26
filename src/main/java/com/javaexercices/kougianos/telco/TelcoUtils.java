233715155e422241d7376f96193d7089ffe3a2c1
Nikos Kougianos <kougi_jr@hotmail.com>
2021-03-26 23:21:35 +0200

    Add TelcoUtils class

---
+++ src/main/java/com/javaexercices/kougianos/telco/TelcoUtils.java
@@ -0,0 +1,40 @@
+package com.javaexercices.kougianos.telco;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class TelcoUtils {
+
+    private TelcoUtils() {
+
+    }
+
+    /**
+     * validate that a msisdn:
+     * - is 10/12 digits long
+     * - starts with 3069 or 69
+     * - all characters are numeric
+     *
+     * @param msisdn String
+     */
+    public static boolean isMsisdnValid(String msisdn) {
+
+        return StringUtils.isNumeric(msisdn)
+                && ((msisdn.startsWith("3069") && msisdn.length() == 12)
+                || (msisdn.startsWith("69") && msisdn.length() == 10));
+
+    }
+
+    /**
+     * validate that a msisdn:
+     * - is 10/12 digits long
+     * - starts with 3069 or 69
+     * - all characters are numeric
+     *
+     * @param cli String
+     */
+    public static boolean isCliValid(String cli) {
+        return StringUtils.isNumeric(cli)
+                && (cli.startsWith("2") && cli.length() == 10);
+    }
+
+}
