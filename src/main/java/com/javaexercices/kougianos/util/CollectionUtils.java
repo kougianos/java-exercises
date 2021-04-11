6cdcc9de05ee5ecf56bb8ad22b826a679b8f227d
Nikos Kougianos <kougi_jr@hotmail.com>
2021-04-11 22:25:51 +0300

    calculateMinNumberInList + unit test

--- src/main/java/com/javaexercices/kougianos/util/CollectionUtils.java
+++ src/main/java/com/javaexercices/kougianos/util/CollectionUtils.java
@@ -1,6 +1,9 @@
 package com.javaexercices.kougianos.util;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class CollectionUtils {
@@ -11,6 +14,7 @@ public class CollectionUtils {
 
     /**
      * Return true if the 2 input lists have at least one element in common, otherwise return false.
+     *
      * @param c1 first list
      * @param c2 second list
      * @return boolean
@@ -29,8 +33,9 @@ public class CollectionUtils {
     /**
      * Convert a primitive array to List. Only supports int[], long[] and double[]. Other arrays (eg float[]) will
      * have an empty collection returned instead.
+     *
      * @param primitiveList input list
-     * @return List
+     * @return List<Number>
      */
     public static List<Number> primitivesToList(Object primitiveList) {
         var x = primitiveList.getClass().getComponentType().getName();
@@ -44,5 +49,41 @@ public class CollectionUtils {
             default:
                 return Collections.emptyList();
         }
+    }
+
+    /**
+     * Returns the element with the minimum value in a list of numbers.
+     * Supported classes:
+     * java.lang.Integer
+     * java.lang.Double
+     * java.lang.Float
+     *
+     * @param numberList input list of numbers
+     * @return Number minimum number
+     * @throws IllegalArgumentException if numberList is empty, or if input class is unsupported.
+     * @throws ClassCastException if numberList contains elements that are not the same type.
+     */
+    public static Number calculateMinNumberInList(List<?> numberList) {
+
+        if (numberList.isEmpty()) {
+            throw new IllegalArgumentException("Input list is empty.");
+        }
+        String c = numberList.get(0).getClass().getName();
+        try {
+            switch (c) {
+                case "java.lang.Integer":
+                    return numberList.stream().mapToInt(e -> (int) e).min().getAsInt();
+                case "java.lang.Double":
+                    return numberList.stream().mapToDouble(e -> (double) e).min().getAsDouble();
+                case "java.lang.Float":
+                    return (float) numberList.stream().mapToDouble(e -> (float) e).min().getAsDouble();
+                default:
+                    throw new IllegalArgumentException("Method supports only Integer, Double, and Float classes");
+            }
+        } catch (ClassCastException e) {
+            throw new ClassCastException("List must contain elements of same type. It cannot contain a mix of " +
+                    "Integer, Float, Double or other Object types.");
+        }
+
     }
 }
--- src/test/java/com/javaexercices/kougianos/util/CollectionUtilsTest.java
+++ src/test/java/com/javaexercices/kougianos/util/CollectionUtilsTest.java
@@ -6,9 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CollectionUtilsTest {
 
@@ -25,10 +23,31 @@ class CollectionUtilsTest {
 
     @Test
     void testPrimitivesToList() {
-
         List<Number> list = CollectionUtils.primitivesToList(new int[]{4, 2, 6, 9});
         assertEquals(9, list.get(list.size() - 1));
         assertEquals(4, list.size());
+    }
 
+    @Test
+    void testCalculateMinNumberInList() {
+        var input1 = Arrays.asList(1, 2, 3, 4, 5, 6, -873, 0);
+        assertEquals(-873, CollectionUtils.calculateMinNumberInList(input1));
+
+        var input2 = Arrays.asList(1.63, 2.1, 3.6, 4.3, 5.8, 6.2, -8873.0, 0.123);
+        assertEquals(-8873.0, CollectionUtils.calculateMinNumberInList(input2));
+
+        var input3 = Arrays.asList(5.34f, 2.56f, -0.129f);
+        assertEquals(-0.129f, CollectionUtils.calculateMinNumberInList(input3));
+
+        var input4 = Arrays.asList("String1", "String2");
+        assertThrows(IllegalArgumentException.class, () -> CollectionUtils.calculateMinNumberInList(input4));
+
+        var input5 = new ArrayList<>();
+        assertThrows(IllegalArgumentException.class, () -> CollectionUtils.calculateMinNumberInList(input5));
+
+        var input6 = new ArrayList<>();
+        input6.add(5);
+        input6.add(5.05);
+        assertThrows(ClassCastException.class, () -> CollectionUtils.calculateMinNumberInList(input6));
     }
 }
