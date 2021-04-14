de2154ade0f87ae8008f2c6b93df3be4ac6a3130
Nikos Kougianos <kougi_jr@hotmail.com>
2021-04-14 23:57:26 +0300

    improve GenericControllerTest

--- src/test/java/com/javaexercices/kougianos/controller/GenericControllerTest.java
+++ src/test/java/com/javaexercices/kougianos/controller/GenericControllerTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,15 +14,15 @@ class GenericControllerTest {
     private final GenericController genericController = new GenericController();
 
     @Test
-    void testXmlToJson() throws IOException {
+    void testXmlToJson() throws FileNotFoundException {
 
         BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/xmlToJsonRequest.xml"));
         String xmlRequest = reader.lines().collect(Collectors.joining("\n"));
 
-        String actualResponse = genericController.convertXmlToJson(xmlRequest).replaceAll("\\s","");
+        String actualResponse = genericController.convertXmlToJson(xmlRequest).replaceAll("\\s", "");
         reader = new BufferedReader(new FileReader("src/test/resources/xmlToJsonResponse.json"));
-        String xmlResponse = reader.lines().collect(Collectors.joining("\n")).replaceAll("\\s","");
-        assertEquals(xmlResponse, actualResponse);
+        String jsonResponse = reader.lines().collect(Collectors.joining("\n")).replaceAll("\\s", "");
+        assertEquals(jsonResponse, actualResponse);
 
     }
 
@@ -31,11 +30,11 @@ class GenericControllerTest {
     void testJsonToXml() throws FileNotFoundException {
 
         BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/jsonToXmlRequest.json"));
-        String xmlRequest = reader.lines().collect(Collectors.joining("\n"));
+        String jsonRequest = reader.lines().collect(Collectors.joining("\n"));
 
-        String actualResponse = genericController.convertJsonToXml(xmlRequest).replaceAll("\\s","");
+        String actualResponse = genericController.convertJsonToXml(jsonRequest).replaceAll("\\s", "");
         reader = new BufferedReader(new FileReader("src/test/resources/jsonToXmlResponse.xml"));
-        String xmlResponse = reader.lines().collect(Collectors.joining("\n")).replaceAll("\\s","");
+        String xmlResponse = reader.lines().collect(Collectors.joining("\n")).replaceAll("\\s", "");
         assertEquals(xmlResponse, actualResponse);
 
     }
