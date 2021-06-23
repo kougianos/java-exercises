742239d83f7184abd5d7f7d15076f1f61cfc5477
Nikos Kougianos <kougi_jr@hotmail.com>
2021-06-24 00:13:13 +0300

    GetValuesFromPropertiesConfig

---
+++ src/main/java/com/javaexercices/kougianos/config/GetValuesFromPropertiesConfig.java
@@ -0,0 +1,6 @@
+package com.javaexercices.kougianos.config;
+
+public class GetValuesFromPropertiesConfig {
+
+    //TODO https://stackoverflow.com/questions/30528255/how-to-access-a-value-defined-in-the-application-properties-file-in-spring-boot
+}
