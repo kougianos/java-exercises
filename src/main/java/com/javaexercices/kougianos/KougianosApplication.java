620d537df4e1bfc5274d7aa752798aa9b8fcaed3
Nikos Kougianos <kougi_jr@hotmail.com>
2021-07-02 20:50:09 +0300

    add tcnative-1.dll for tomcat

--- src/main/java/com/javaexercices/kougianos/KougianosApplication.java
+++ src/main/java/com/javaexercices/kougianos/KougianosApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@SpringBootApplication(exclude = {
+        MongoAutoConfiguration.class,
+        MongoDataAutoConfiguration.class
+})
 public class KougianosApplication {
 
     public static void main(String[] args) {
