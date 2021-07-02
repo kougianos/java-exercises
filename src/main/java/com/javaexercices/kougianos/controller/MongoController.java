901d1f4bd15d8f5613ce9af8a3e6f3640e82ca3d
Nikos Kougianos <kougi_jr@hotmail.com>
2021-07-03 00:20:37 +0300

    create MongoController

---
+++ src/main/java/com/javaexercices/kougianos/controller/MongoController.java
@@ -0,0 +1,13 @@
+package com.javaexercices.kougianos.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "")
+@Slf4j
+public class MongoController {
+
+    //TODO
+}
