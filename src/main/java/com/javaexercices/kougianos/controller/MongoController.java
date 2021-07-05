193fc9737f21e0acdeb42fd92a5e8edb39632fdf
Nikos Kougianos <kougi_jr@hotmail.com>
2021-07-05 10:08:20 +0300

    update initial paths for mongo and sql controllers

--- src/main/java/com/javaexercices/kougianos/controller/MongoController.java
+++ src/main/java/com/javaexercices/kougianos/controller/MongoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "")
+@RequestMapping(path = "mongo")
 @Slf4j
 public class MongoController {
     //TODO
--- src/main/java/com/javaexercices/kougianos/controller/SqlController.java
+++ src/main/java/com/javaexercices/kougianos/controller/SqlController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "")
+@RequestMapping(path = "sql")
 @Slf4j
 public class SqlController {
     //TODO
